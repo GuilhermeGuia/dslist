81449248d49bbab7525d6af9d2537aa57caf0934
Guilherme Guia <guilhermeguia007@gmail.com>
2023-11-14 21:59:28 -0400

    Add listing gameList

---
+++ src/main/java/com/guilherme/dslist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.guilherme.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.dslist.dto.GameListDTO;
+import com.guilherme.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> lists = this.gameListService.findAll();
+        return lists;
+    }
+}
---
+++ src/main/java/com/guilherme/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.guilherme.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.dslist.dto.GameListDTO;
+import com.guilherme.dslist.entities.GameList;
+import com.guilherme.dslist.repositories.GameListRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional
+    public List<GameListDTO> findAll() {
+        List<GameList> result = this.gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+}
